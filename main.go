5202362f5e6940dbc49d729e4c4469bbff06e94b
ilDug <dognini.marco@gmail.com>
2024-12-28 17:35:43 +0100

    Select the command in GO

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"cre/core"
 	"cre/styles"
 	"fmt"
 )
 
 
 func main() {
-	fmt.Println(styles.TitleSyle.Render("Credentials Manager"))
+	fmt.Print(styles.TitleStyle.Render("Credentials Manager"))
 
-	
+    var command string = core.SelectCommand()
 
+	switch command {
+		case "MONGO":
+			fmt.Println(styles.CommandStyle.Render("MongoDB Utilities... creating mongo credentials"))
+		case "CERTIFICATE":
+			fmt.Println(styles.CommandStyle.Render("Certificate Manager... generating certificate/key pair"))
+	}
 }
--- styles/styles.go
+++ styles/styles.go
@@ -1,14 +1,29 @@
 package styles
 
 import (
+	"os"
+
 	"github.com/charmbracelet/lipgloss"
+	"golang.org/x/term"
 )
 
-var TitleSyle = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#FAFAFA")).
-    Background(lipgloss.Color("#675499")).
+var termWidth, _, _  = term.GetSize(int(os.Stdout.Fd()))
+
+var TitleStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#675499")).
 	Padding(1).
-	Margin(1,0).
-	Width(50).
+	Margin(1, 0).
+	Width(termWidth).
 	Align(lipgloss.Center)
+
+	
+
+var CommandStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#a4a8b0")).
+	Margin(1, 0).
+	MarginTop(0).
+	Width(50).
+	Align(lipgloss.Left)
+	
